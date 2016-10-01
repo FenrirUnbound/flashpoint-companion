b62b0a8e700467c683440be9675ddb1de37eaa35
Darren M <aeneascorrupt@gmail.com>
2016-10-01 15:36:18 -0700

    reroll specific elements

--- main.js
+++ main.js
@@ -14,15 +14,15 @@ var FIRST_EXPLOSION = [
   [4, 4],
   [4, 3]
 ];
-var EIGHTD_MAP = {
-  1: 6,
-  2: 5,
-  3: 8,
-  4: 7,
-  5: 2,
-  6: 1,
-  7: 4,
-  8: 3
+var D8_MAP = {
+  '1': 6,
+  '2': 5,
+  '3': 8,
+  '4': 7,
+  '5': 2,
+  '6': 1,
+  '7': 4,
+  '8': 3
 };
 
 
@@ -31,19 +31,13 @@ main.controller('ScenarioController', ['$scope', '$http', ($scope, $http) => {
     return Math.floor(Math.random() * sides) + 1;
   }
 
+
+
   $scope.firstExplosion = FIRST_EXPLOSION[rollDice(8)];
 
-  $scope.secondExplosion = [
-    [rollDice(6), rollDice(8)],
-    [rollDice(6), rollDice(8)],
-    [rollDice(6), rollDice(8)]
-  ];
+  $scope.secondExplosion = [rollDice(6), rollDice(8)];
 
-  $scope.thirdExplosion = [
-    [ rollDice(6), EIGHTD_MAP[$scope.secondExplosion[0][1]] ],
-    [ rollDice(6), EIGHTD_MAP[$scope.secondExplosion[1][1]] ],
-    [ rollDice(6), EIGHTD_MAP[$scope.secondExplosion[2][1]] ],
-  ];
+  $scope.thirdExplosion = [ rollDice(6), D8_MAP[$scope.secondExplosion[1]] ];
 
   $scope.hazMat = [
     [rollDice(6), rollDice(8)],
@@ -71,4 +65,30 @@ main.controller('ScenarioController', ['$scope', '$http', ($scope, $http) => {
     [rollDice(6), rollDice(8)],
     [rollDice(6), rollDice(8)]
   ];
+
+  function rerollThirdExplosion() {
+    var blackDice = D8_MAP[$scope.secondExplosion[1]];
+    var redDice = rollDice(6);
+
+    return [redDice, blackDice];
+  }
+
+  $scope.rerollExplosion = function (gameElement) {
+    var newRoll = [rollDice(6), rollDice(8)];
+
+    if (gameElement === 'thirdExplosion') {
+      newRoll = rerollThirdExplosion();
+    } else if (gameElement === 'secondExplosion') {
+      $scope[gameElement] = newRoll;
+      $scope.thirdExplosion = rerollThirdExplosion();
+    }
+
+    $scope[gameElement] = newRoll;
+  };
+
+  $scope.reroll = function (gameElement, index) {
+    var targetIndex = parseInt(index);
+
+    $scope[gameElement][targetIndex] = [rollDice(6), rollDice(8)];
+  };
 }]);
